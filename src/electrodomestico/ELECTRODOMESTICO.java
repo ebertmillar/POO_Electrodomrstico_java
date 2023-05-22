f23fc1dc927fbf7cf8a984ce92701450bc54be29
unknown <ebertmillar@gmail.com>
2023-05-22 12:48:09 +0200

    segundo commit

--- src/electrodomestico/ELECTRODOMESTICO.java
+++ src/electrodomestico/ELECTRODOMESTICO.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package electrodomestico;
 
@@ -8,13 +8,6 @@ package electrodomestico;
  *
  * @author Ebert
  */
-public class ELECTRODOMESTICO {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Electrodomestico {
     
 }
